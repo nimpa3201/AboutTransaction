2e34922c725e681c9b22e44083d97a67ff5e4b68
gwongilnam <nimpa3201@naver.com>
2025-03-07 05:12:45 +0900

    feat: 트랜잭션 활용 - 커밋,롤백

---
+++ src/test/java/com/example/springtx/propagation/MemberServiceTest.java
@@ -0,0 +1,69 @@
+package com.example.springtx.propagation;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@Slf4j
+@SpringBootTest
+class MemberServiceTest {
+
+    @Autowired
+    MemberService memberService;
+    @Autowired
+    MemberRepository memberRepository;
+    @Autowired
+    LogRepository logRepository;
+
+
+    /**
+     * memberService    @Transactionl : OFF
+     * memberRepository @Transactionl : ON
+     * logRepository   @Transactionl : ON
+     */
+    @Test
+    void outerTxOff_success() {
+
+        //given
+        String username = " outerTxOff_success";
+
+
+        //when
+        memberService.joinV1(username);
+
+        //then
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+
+    }
+
+
+    /**
+     * memberService    @Transactionl : OFF
+     * memberRepository @Transactionl : ON
+     * logRepository   @Transactionl : ON Excption
+     */
+    @Test
+    void outerTxOff_fail() {
+
+        //given
+        String username = "로그예외_outerTxOff_fail";
+
+
+        //when
+        org.assertj.core.api.Assertions.assertThatThrownBy(() -> memberService.joinV1(username))
+            .isInstanceOf(RuntimeException.class);
+
+        //then
+        assertTrue(memberRepository.find(username).isPresent()); // memger 저장( 커밋 성공)
+        assertTrue(logRepository.find(username).isEmpty()); // 롤백
+
+    }
+
+
+}
